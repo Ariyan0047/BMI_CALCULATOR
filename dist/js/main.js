88958783099e34773344e7be7c3b988a5d21f8f8
Ariyan0047 <aariyan0047@gmail.com>
2020-02-16 21:36:12 +0600

    .

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+const height = document.querySelector(".height");
+const weight = document.querySelector(".weight");
+const bmi = document.querySelector(".bmi");
+const btn = document.querySelector(".btn1");
+const btn1 = document.querySelector(".btn2");
+
+btn.addEventListener("click", Converter);
+
+function Converter() {
+  if (height.value <= 0 || weight.value <= 0) {
+    alert(`Value Should Be Greater Than 0`);
+  } else if ((height.value = "" && weight.value <= "")) {
+    alert(`Fields Can't Be Empty`);
+  } else {
+    const newHeight = parseFloat(height.value);
+    const newWeight = parseFloat(weight.value);
+    const newBmi = newWeight / (newHeight * newHeight).toFixed(2);
+    bmi.value = newBmi.toFixed(2);
+  }
+}
+
+btn1.addEventListener("click", Reset);
+
+function Reset() {
+  height.value = 0;
+  weight.value = 0;
+  bmi.value = 0;
+}
