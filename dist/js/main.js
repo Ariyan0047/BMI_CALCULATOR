5d450170c16089db242a3cb3b9855f5a5aa8e6f8
Ariyan0047 <aariyan0047@gmail.com>
2020-02-17 09:10:32 +0600

    .

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,15 +7,15 @@ const btn1 = document.querySelector(".btn2");
 btn.addEventListener("click", Converter);
 
 function Converter() {
-  if (height.value <= 0 || weight.value <= 0) {
-    if ((height.value === "" || weight.value === "")) {
-      alert(`Fields Can't Be Empty And Value's Must Be Greater Than 0`);
-    }
-  } else {
+  if (height.value > 0 && weight.value > 0) {
     const newHeight = parseFloat(height.value);
     const newWeight = parseFloat(weight.value);
     const newBmi = newWeight / (newHeight * newHeight).toFixed(2);
     bmi.value = newBmi.toFixed(2);
+  } else if ((height.value === "" || weight.value === "")) {
+    alert(`Fields Can't Be Empty`);
+  } else {
+    alert(`Values must be greater than 0`)
   }
 }
 
